ffbcd2214229220fdad18ca1f09241c11e298670
param pam pam <arifamjy@gmail.com>
2023-02-10 11:57:31 +0700

    chore: setup cors for domain

--- backend/src/utils/allowedOrigins.ts
+++ backend/src/utils/allowedOrigins.ts
@@ -1,3 +1,6 @@
-const ALLOWED_ORIGINS = ['http://localhost:3000']
+const ALLOWED_ORIGINS = [
+  'http://localhost:3000',
+  'https://teuxdeux-clone-fullstack-production.up.railway.app/',
+]
 
 export default ALLOWED_ORIGINS
